c3471a9ff5e55df0ad9011b129af61d0b7f1bbbd
expki <expki@vdh.dev>
2025-03-02 11:19:39 +1300

    Latest bundles

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ if ! command -v wget &> /dev/null; then
     sudo apt update && sudo apt install wget -y
 fi
 if [ ! -f static/swagger-ui.css ]; then
-    wget https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css -O static/swagger-ui.css
+    wget https://unpkg.com/swagger-ui-dist@latest/swagger-ui.css -O static/swagger-ui.css
 fi
 if [ ! -f static/swagger-ui-bundle.js ]; then
-    wget https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js -O static/swagger-ui-bundle.js
+    wget https://unpkg.com/swagger-ui-dist@latest/swagger-ui-bundle.js -O static/swagger-ui-bundle.js
 fi
 GOOS=linux GOARCH=amd64 go build -tags='avx' -o gocvd .
 if command -v nvcc &> /dev/null; then
