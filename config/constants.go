c112e7dc226c27a41242569cc58fa7a8697c3941
expki <expki@vdh.dev>
2025-03-16 13:28:12 +1300

    Updated constants

--- config/constants.go
+++ config/constants.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	BATCH_SIZE_DATABASE          = 1_000
-	MAX_CENTROID_SIZE            = 2 //10_000
+	MAX_CENTROID_SIZE            = 10_000
 	CENTROID_REBALANCE_THRESHOLD = 500
-	CENTROID_REFRESH_INTERVAL    = time.Minute
+	CENTROID_REFRESH_INTERVAL    = 5 * time.Minute
 )
