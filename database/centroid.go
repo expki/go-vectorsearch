609deaa9246d7b83fe633bbcce0cfc3f6272fa8e
expki <expki@vdh.dev>
2025-03-19 01:48:51 +1300

    Implemented k-means

--- database/centroid.go
+++ database/centroid.go
@@ -3,17 +3,14 @@ package database
 import (
 	"context"
 	"errors"
-	"math"
 	"os"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/expki/go-vectorsearch/compute"
 	"github.com/expki/go-vectorsearch/config"
 	"github.com/expki/go-vectorsearch/logger"
 	"github.com/schollz/progressbar/v3"
-	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 	"gorm.io/plugin/dbresolver"
 )
@@ -40,274 +37,74 @@ func (d *Database) refreshCentroidJob(appCtx context.Context) {
 }
 
 func (d *Database) refreshCentroids(appCtx context.Context) {
-	logger.Sugar().Info("Starting centroids refresh job")
-	order := clause.OrderByColumn{
-		Column: clause.Column{Name: "last_updated"},
-	}
-	var centroids []Centroid
-	err := d.WithContext(appCtx).Clauses(dbresolver.Read).Order(order).Find(&centroids).Error
-	if err != nil {
-		logger.Sugar().Errorw("Failed to fetch centroids", "error", err)
-		return
-	}
-	bar := progressbar.Default(int64(len(centroids)), "Refreshing centroids")
-	for _, centroid := range centroids {
-		d.refreshCentroid(appCtx, centroid)
-		bar.Add(1)
-	}
-
-	// check if it still needs to be refreshed
-	if d.provider == config.DatabaseProvider_PostgreSQL {
-		for {
-			var maxCount int
-			err := d.DB.WithContext(appCtx).Clauses(dbresolver.Write).Raw(`
-	SELECT COUNT(d.id) AS dc_count
-	FROM centroids AS c
-	LEFT JOIN documents AS d
-	ON d.centroid_id = c.id
-	GROUP BY c.id, c.last_updated
-	ORDER BY dc_count DESC
-	LIMIT 1;`).Scan(&maxCount).Error
-			if err == nil {
-			} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-				break
-			} else {
-				logger.Sugar().Errorw("Failed re-refresh centroid", "error", err)
-				break
-			}
-			if maxCount <= config.MAX_CENTROID_SIZE {
-				break
-			}
-			bar.AddMax(len(centroids))
-			for _, centroid := range centroids {
-				d.refreshCentroid(appCtx, centroid)
-				bar.Add(1)
-			}
-		}
-	}
-
-	bar.Close()
-	logger.Sugar().Info("Finished centroids refresh job")
-}
-
-func (d *Database) refreshCentroid(appCtx context.Context, centroid Centroid) {
-	// Lock centroid for writing
-	tx := d.WithContext(appCtx).Clauses(dbresolver.Write)
-	if d.provider == config.DatabaseProvider_PostgreSQL {
-		tx = tx.Begin()
-		tx = tx.Clauses(clause.Locking{
-			Strength: "SHARE",
-			Table:    clause.Table{Name: clause.CurrentTable},
-			Options:  "NOWAIT",
-		})
-	}
-	err := tx.Take(&centroid, centroid.ID).Error
+	// Retrieve list of categories
+	var categories []Category
+	err := d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Select("id").Find(&categories).Error
 	if err == nil {
-		logger.Sugar().Debug("Starting centroid refresh job", "centroid_id", centroid.ID)
 	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		tx.Rollback()
-		return
-	} else if strings.Contains(err.Error(), "lock") {
-		// ignore if other services is already refreshing
-		tx.Rollback()
+		logger.Sugar().Info("Refresh centroids cancelled")
 		return
 	} else {
-		tx.Rollback()
-		logger.Sugar().Errorw("Failed to lock centroid", "error", err)
+		logger.Sugar().Errorw("Failed to retrieve list of categories", "error", err)
 		return
 	}
 
-	// split centroid
-	centroid, newCentroids := d.splitCentroid(appCtx, centroid)
-
-	// re-balance centroid across splitted centroids
-	if len(newCentroids) > 0 {
-		d.reBalanceCentroids(appCtx, centroid, newCentroids)
-	}
-
-	// Unlock centroid
-	logger.Sugar().Debug("Centroid refreshed", "centroid_id", centroid.ID)
-	if d.provider == config.DatabaseProvider_PostgreSQL {
-		tx.Commit()
-	}
-
-	// re-center original and splitted centroids
-	for _, newCentroid := range newCentroids {
-		err = d.reCenterCentroid(appCtx, newCentroid)
+	// Process each item one by one with row-level locking
+	bar := progressbar.Default(int64(len(categories)), "Processing categories")
+	for _, category := range categories {
+		// Lock category
+		tx := d.DB.WithContext(appCtx).Clauses(dbresolver.Write).Begin() // Start a transaction
+		if d.provider == config.DatabaseProvider_PostgreSQL {
+			tx = tx.Begin()
+			tx = tx.Clauses(clause.Locking{
+				Strength: "SHARE",
+				Table:    clause.Table{Name: clause.CurrentTable},
+				Options:  "NOWAIT",
+			})
+		}
+		err = tx.First(&category, category.ID).Error
 		if err == nil {
 		} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-			tx.Rollback()
+			logger.Sugar().Info("Refresh centroids cancelled")
+			if d.provider == config.DatabaseProvider_PostgreSQL {
+				tx.Rollback()
+			}
+			return
+		} else if strings.Contains(strings.ToLower(err.Error()), "lock") {
+			if d.provider == config.DatabaseProvider_PostgreSQL {
+				tx.Rollback()
+			}
+			bar.Add(1)
+			continue // skip if another instance is already processing the category
+		} else {
+			logger.Sugar().Errorw("Failed to retrieve list of categories", "error", err)
+			if d.provider == config.DatabaseProvider_PostgreSQL {
+				tx.Rollback()
+			}
+			return
+		}
+
+		// Process category
+		err = d.KMeansCentroidAssignment(appCtx, category.ID)
+		if err == nil {
+		} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+			logger.Sugar().Info("Refresh centroids cancelled")
+			if d.provider == config.DatabaseProvider_PostgreSQL {
+				tx.Rollback()
+			}
 			return
 		} else {
-			tx.Rollback()
-			logger.Sugar().Errorw("Failed to re-center centroid", "error", err)
+			logger.Sugar().Errorw("Failed to process category", "error", err)
+			if d.provider == config.DatabaseProvider_PostgreSQL {
+				tx.Rollback()
+			}
 			return
 		}
-	}
-	err = d.reCenterCentroid(appCtx, centroid)
-	if err == nil {
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		tx.Rollback()
-		return
-	} else {
-		tx.Rollback()
-		logger.Sugar().Errorw("Failed to re-center centroid", "error", err)
-		return
-	}
-}
 
-func (d *Database) splitCentroid(appCtx context.Context, centroid Centroid) (original Centroid, newCentroids []Centroid) {
-	// get centroid document count
-	var countDocuments int64
-	err := d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Model(&Document{}).Where("centroid_id = ?", centroid.ID).Count(&countDocuments).Error
-	if err == nil {
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		return
-	} else {
-		logger.Sugar().Errorw("Failed to count documents for centroid", "error", err)
-		return
-	}
-
-	// check if centroid needs to be split
-	if countDocuments <= config.MAX_CENTROID_SIZE {
-		return
-	}
-
-	// calculate new centroid count
-	newCentroidCount := int(math.Ceil(float64(countDocuments)/float64(config.MAX_CENTROID_SIZE))) - 1
-
-	// fetch new centroid documents
-	var newCentroidDocuments []Document
-	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Where("centroid_id = ?", centroid.ID).Select("id", "vector").Order("RANDOM()").Limit(newCentroidCount).Find(&newCentroidDocuments).Error
-	if err == nil {
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		return
-	} else {
-		logger.Sugar().Errorw("Failed to fetch new centroid documents", "error", err)
-		return
-	}
-
-	// create new centroids
-	newCentroids = make([]Centroid, newCentroidCount)
-	for idx := range newCentroidCount {
-		newCentroids[idx].Vector = newCentroidDocuments[idx].Vector
-		newCentroids[idx].LastUpdated = time.Time{}
-		newCentroids[idx].CategoryID = centroid.CategoryID
-		newCentroids[idx].Category = centroid.Category
-	}
-	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Write).Create(&newCentroids).Error
-	if err == nil {
-		centroid.LastUpdated = time.Time{}
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		return
-	} else {
-		logger.Sugar().Errorw("Failed to create new centroids", "error", err)
-		return
-	}
-
-	return centroid, newCentroids
-}
-
-func (d *Database) reBalanceCentroids(appCtx context.Context, centroid Centroid, newCentroids []Centroid) {
-	// create new centroid matrix
-	newCentroidMatrix := make([][]uint8, len(newCentroids)+1)
-	newCentroidMatrix[0] = centroid.Vector
-	for idx, centroid := range newCentroids {
-		newCentroidMatrix[idx+1] = centroid.Vector
-	}
-	matrixCentroids := compute.NewMatrix(newCentroidMatrix)
-
-	// fetch documents for original centroid
-	var documents []Document
-	err := d.DB.WithContext(appCtx).Clauses(dbresolver.Write).Where("centroid_id = ?", centroid.ID).Select("id", "vector").FindInBatches(&documents, config.BATCH_SIZE_DATABASE, func(tx *gorm.DB, batch int) (err error) {
-		// find closest centroids
-		documentMatrix := make([][]uint8, len(documents))
-		for idx, document := range documents {
-			documentMatrix[idx] = document.Vector
+		// Unlock category
+		if d.provider == config.DatabaseProvider_PostgreSQL {
+			tx.Commit()
 		}
-		_, centroidIdList := matrixCentroids.Clone().CosineSimilarity(compute.NewMatrix(documentMatrix))
-
-		// group documents by nearest centroid
-		centroidDocumentsMap := make(map[uint64][]uint64, len(newCentroids))
-		for _, centroid := range newCentroids {
-			centroidDocumentsMap[centroid.ID] = make([]uint64, 0, len(documents))
-		}
-		for idx, centroidIdx := range centroidIdList {
-			if centroidIdx == 0 {
-				continue // already in correct centroid
-			}
-			document := documents[idx]
-			newCentroidIdx := centroidIdx - 1
-			newCentroid := newCentroids[newCentroidIdx]
-			centroidDocumentsMap[newCentroid.ID] = append(centroidDocumentsMap[newCentroid.ID], document.ID)
-		}
-
-		// update document centroid id
-		for centroidId, documentIdList := range centroidDocumentsMap {
-			err = tx.Model(&Document{}).Where("id IN ?", documentIdList).Update("centroid_id", centroidId).Error
-			if err != nil {
-				return errors.Join(errors.New("failed to update document centroid id"), err)
-			}
-		}
-		return nil
-	}).Error
-	if err == nil {
-		// centroids found and handled
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		// centroids request canceled
-		return
-	} else {
-		// centroids retrieve error
-		logger.Sugar().Errorw("failed to retrieve centroid documents", "error", err)
-		return
+		bar.Add(1)
 	}
-}
-
-func (d *Database) reCenterCentroid(appCtx context.Context, centroid Centroid) error {
-	// Check if centroid requries update
-	var latestDocument Document
-	err := d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Where("centroid_id = ?", centroid.ID).Order("last_updated DESC").Take(&latestDocument).Error
-	if err == nil {
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		return err
-	}
-	if latestDocument.LastUpdated.Before(centroid.LastUpdated) {
-		return nil
-	}
-
-	// get centroid documents
-	var documents []Document
-	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Where("centroid_id = ?", centroid.ID).Select("id", "vector").Limit(config.MAX_CENTROID_SIZE).Find(&documents).Error
-	if err == nil || errors.Is(err, gorm.ErrRecordNotFound) {
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		return err
-	} else {
-		return errors.Join(errors.New("failed to find documents"), err)
-	}
-	if len(documents) == 0 {
-		return nil
-	}
-
-	// center centroid
-	centerVector := make([]float32, len(documents[0].Vector))
-	for _, document := range documents {
-		for i, v := range compute.DequantizeVector(document.Vector.Underlying(), -1, 1) {
-			centerVector[i] += v
-		}
-	}
-	for i := range centerVector {
-		centerVector[i] /= float32(len(documents))
-	}
-
-	// update centroid vector
-	centroid.LastUpdated = time.Now().UTC()
-	centroid.Vector = compute.QuantizeVector(centerVector, -1, 1)
-	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Write).Save(&centroid).Error
-	if err == nil {
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
-		return err
-	} else {
-		return errors.Join(errors.New("failed to update centroid"), err)
-	}
-	return nil
 }
---
+++ database/k_means.go
@@ -0,0 +1,201 @@
+package database
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"math"
+	"os"
+
+	"github.com/expki/go-vectorsearch/compute"
+	"github.com/expki/go-vectorsearch/config"
+	"github.com/schollz/progressbar/v3"
+	"gorm.io/gorm"
+	"gorm.io/plugin/dbresolver"
+)
+
+func (d *Database) KMeansCentroidAssignment(appCtx context.Context, categoryID uint64) (err error) {
+	// Calculate k centroids
+	var countDocuments int64
+	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Model(&Document{}).Where("category_id = ?", categoryID).Count(&countDocuments).Error
+	if err == nil {
+	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+		return err
+	} else {
+		return errors.Join(errors.New("failed to count documents"), err)
+	}
+	k := int(math.Ceil(float64(countDocuments) / float64(config.MAX_CENTROID_SIZE)))
+
+	// Get current centroids
+	var centroids []Centroid
+	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Where("category_id = ?", categoryID).Find(&centroids).Error
+	if err == nil {
+	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+		return err
+	} else {
+		return errors.Join(errors.New("failed to get current centroids"), err)
+	}
+	if len(centroids) <= k { // already have enough centroids
+		return nil
+	}
+
+	// Add new random centroids
+	var randomDocuments []Document
+	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Where("category_id = ?", categoryID).Select("vector").Order("RANDOM()").Limit(k - len(centroids)).Find(&randomDocuments).Error
+	if err == nil {
+	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+		return
+	} else {
+		return errors.Join(errors.New("failed to get random documents"), err)
+	}
+	for _, doc := range randomDocuments {
+		centroids = append(centroids, Centroid{
+			Vector:     doc.Vector,
+			CategoryID: categoryID,
+		})
+	}
+
+	// Loop until convergence
+	bar := progressbar.Default(0, "K-Means Clustering")
+	var newMatrixQuantizedCentroids [][]uint8
+	var converged bool
+	for !converged {
+		bar.AddMax64(countDocuments)
+
+		// convert centroids to matrix
+		matrixQuantizedCentroids := make([][]uint8, k)
+		for idx, centroid := range centroids {
+			matrixQuantizedCentroids[idx] = centroid.Vector
+		}
+		matrixCentroids := compute.NewMatrix(matrixQuantizedCentroids)
+
+		// initialize new centroids
+		newCentroidsMeanVectors := make([][]float32, k)
+		newCentroidsSumVectors := make([][]float32, k)
+		newCentroidsDocumentCount := make([]int, k)
+		for idx := range newCentroidsSumVectors {
+			newCentroidsMeanVectors[idx] = make([]float32, len(matrixQuantizedCentroids[0]))
+			newCentroidsSumVectors[idx] = make([]float32, len(matrixQuantizedCentroids[0]))
+		}
+
+		// retrieve category documents in batches
+		var documents []Document
+		err = d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Where("category_id = ?", categoryID).Select("id", "vector").FindInBatches(&documents, config.BATCH_SIZE_DATABASE, func(tx *gorm.DB, batch int) (err error) {
+			// convert documents to matrix
+			documentQuantizedMatrix := make([][]uint8, len(documents))
+			for idx, document := range documents {
+				documentQuantizedMatrix[idx] = document.Vector
+			}
+			matrixDocuments := compute.NewMatrix(documentQuantizedMatrix)
+
+			// calculate nearest centroids for each document
+			_, centroidIdList := matrixCentroids.Clone().CosineSimilarity(matrixDocuments)
+
+			// accumulate vectors and count documents for new centroids
+			for idx, centroidIdx := range centroidIdList {
+				documentVector := compute.DequantizeVector(documents[idx].Vector, -1, 1)
+				for j, val := range documentVector {
+					newCentroidsSumVectors[centroidIdx][j] += val
+				}
+				newCentroidsDocumentCount[centroidIdx]++
+			}
+
+			bar.Add(len(documents))
+			return nil
+		}).Error
+		if err == nil {
+		} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+			return err
+		} else {
+			return errors.Join(errors.New("failed to calculate nearest centroids for documents"), err)
+		}
+
+		// calculate mean vectors for new centroids
+		for idx, count := range newCentroidsDocumentCount {
+			for j, total := range newCentroidsSumVectors[idx] {
+				newCentroidsMeanVectors[idx][j] = total / float32(count)
+			}
+		}
+
+		// quantize new centroids
+		newMatrixQuantizedCentroids = compute.QuantizeMatrix(newCentroidsMeanVectors, -1, 1)
+
+		// check if converged
+		for idx, newCentroid := range newMatrixQuantizedCentroids {
+			centroid := matrixQuantizedCentroids[idx]
+			converged = bytes.Equal(newCentroid, centroid)
+			if !converged {
+				break
+			}
+		}
+	}
+	bar.Close()
+
+	// Update centroid vectors
+	for idx, vector := range newMatrixQuantizedCentroids {
+		centroids[idx].Vector = vector
+	}
+
+	// Save new centroids in database
+	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Write).Save(&centroids).Error
+	if err == nil {
+	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+		return err
+	} else {
+		return errors.Join(errors.New("failed to save new/updated centroids in database"), err)
+	}
+
+	// Assign documents to new centroids
+	bar = progressbar.Default(int64(countDocuments), "Reassigning Documents to New Centroids")
+	matrixCentroids := compute.NewMatrix(newMatrixQuantizedCentroids)
+	var documents []Document
+	err = d.DB.WithContext(appCtx).Clauses(dbresolver.Read).Where("category_id = ?", categoryID).Select("id", "centroid_id").FindInBatches(&documents, config.BATCH_SIZE_DATABASE, func(tx *gorm.DB, batch int) (err error) {
+		// convert documents to matrix
+		documentQuantizedMatrix := make([][]uint8, len(documents))
+		for idx, document := range documents {
+			documentQuantizedMatrix[idx] = document.Vector
+		}
+		matrixDocuments := compute.NewMatrix(documentQuantizedMatrix)
+
+		// calculate nearest centroids for each document
+		_, centroidIdList := matrixCentroids.Clone().CosineSimilarity(matrixDocuments)
+
+		// assign documents to new centroids
+		centroidDocuments := make([][]uint64, k)
+		for documentIdx, centroidIdx := range centroidIdList {
+			centroid := centroids[centroidIdx]
+			document := documents[documentIdx]
+			if document.CentroidID == centroid.ID {
+				bar.Add(1)
+				continue
+			}
+			centroidDocuments[centroidIdx] = append(centroidDocuments[centroidIdx], document.ID)
+		}
+
+		// update document centroids in database
+		for centroidIdx, documentIds := range centroidDocuments {
+			if len(documentIds) == 0 {
+				continue
+			}
+			centroid := centroids[centroidIdx]
+			err = tx.Model(&Document{}).Where("id IN ?", documentIds).Update("centroid_id", centroid.ID).Error
+			if err == nil {
+			} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+				return err
+			} else {
+				return errors.Join(errors.New("failed to update document centroids in database"), err)
+			}
+
+			bar.Add(len(centroidDocuments))
+		}
+		return nil
+	}).Error
+	if err == nil {
+	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) || errors.Is(err, os.ErrDeadlineExceeded) {
+		return err
+	} else {
+		return errors.Join(errors.New("failed to calculate nearest centroids for documents"), err)
+	}
+
+	return nil
+}
